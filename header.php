a3610cfc9da1bb7f729549cc8209bcaa17ba59a4
Ryan <ryantjosephson@gmail.com>
2020-02-11 15:16:13 -0700

    add

---
+++ header.php
@@ -0,0 +1,13 @@
+<?php
+<div class="banner">
+	<div class="logo">
+	<a href="www.google.com"><img src="logofavicon.png">
+	</div>
+	<div class="login">
+		<div class="userbutton">
+		</div>
+		<div class="passwordbutton">
+		</div>
+	</div>
+</div>
+?>

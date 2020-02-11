e3979c6d96347781f803932c80630436fe0d37e3
Ryan <ryantjosephson@gmail.com>
2020-02-11 15:30:23 -0700

    add

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
 <div class="banner">
 	<div class="logo">
-	<a href="www.google.com"><img src="logofavicon.png">
+	<a href="www.google.com"><img src="logofavicon.png" height="200px"></a>
 	</div>
 	<div class="login">
 		<div class="userbutton">

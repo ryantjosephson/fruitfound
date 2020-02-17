64e80e18f199dd7782609178ca44d61b589537e2
Ryan <ryantjosephson@gmail.com>
2020-02-17 15:50:37 -0700

    new

--- header.php
+++ header.php
@@ -6,8 +6,8 @@
 	<div class="login">
 		<div class="formcontainer">
 			<form method="POST" action="addlocation.php">
-				<div> Username: <input type="text" id="username" name="username"> </div>
-				<div> Password: <input type="text" id="password" name="password"> </div>
+				<div class="logindiv"> Username: <input type="text" id="username" name="username"> </div>
+				<div class="logindiv"> Password: <input type="text" id="password" name="password"> </div>
 			</form>
 		</div>
 	

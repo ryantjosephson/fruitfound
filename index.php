eb11bab5267f99e8118eff7fabbb1bc18fd29899
Ryan <ryantjosephson@gmail.com>
2020-02-17 16:25:10 -0700

    new

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 	<title>Hello World</title>
 	<link rel="stylesheet" type="text/css" href="header.css">
 	<link rel="stylesheet" type="text/css" href="body.css">
+	body{
+	background-color: green;
+	margin: 0;
+	padding: 5px;
+}
 	</header>
 <body>
 	<?php 

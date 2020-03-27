8c736f33769099e719656ad4856d64be6be1ee02
Ryan <ryantjosephson@gmail.com>
2020-03-27 10:56:07 -0600

    new

--- header.php
+++ header.php
@@ -1,11 +1,10 @@
 
 <header class="header">
-
 	<div class="header-left">
-	<a href="index.php"><img class="logoimg" src="appletree.png"></a>
-	<h1 class="titletext">FruitFound!</h1>
+		<a href="index.php"><img class="logoimg" src="appletree.png"></a>
+		<h1 class="titletext">FruitFound!</h1>
 	</div>
 	<div class="header-right">
-		<a href="login.php">Login</a>
+		<a class="loginpage" href="login.php">Login</a>
 	</div>
 </header>

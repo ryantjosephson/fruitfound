0baa24e095a8870dbfe3a18b12d294ba45209597
Ryan <ryantjosephson@gmail.com>
2020-03-27 12:56:29 -0600

    new

--- loginhandler.php
+++ loginhandler.php
@@ -2,6 +2,8 @@
   session_start();
 	require_once 'dao.php';
 	
+	$connection = new Dao();
+	
 	$stmt = $connection->prepare("SELECT * FROM users WHERE  = ?");
 	$stmt->execute([$_POST['username']]);
 	$user = $stmt->fetch();

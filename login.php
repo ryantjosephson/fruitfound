64f44ba6b3bd973a8635265dc0b0b10b74617c46
Ryan <ryantjosephson@gmail.com>
2020-03-27 11:25:54 -0600

    new

--- login.php
+++ login.php
@@ -6,9 +6,10 @@
 	<html>
 		<head>
 			<link rel="stylesheet" type="text/css" href="header.css">
-				<link rel="stylesheet" type="text/css" href="main.css">
+			<link rel="stylesheet" type="text/css" href="main.css">
 			<link rel="stylesheet" type="text/css" href="footer.css">
 			<link rel="stylesheet" type="text/css" href="login.css">
+			<link href="https://fonts.googleapis.com/css?family=Londrina+Solid&display=swap" rel="stylesheet">
 			
 		</head>
 	<body>

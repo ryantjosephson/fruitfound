c5397b0a839d24ab45a558090ac002b2fa0bef1a
Ryan <ryantjosephson@gmail.com>
2020-03-27 09:51:52 -0600

    new

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 	<header>
-	<title>Hello World</title>
+	<title>FruitFound</title>
 	<link rel="stylesheet" type="text/css" href="header.css">
 	<link rel="stylesheet" type="text/css" href="main.css">
 	<link rel="stylesheet" type="text/css" href="footer.css">
---
+++ login.php
@@ -0,0 +1,17 @@
+	<?php 
+		session_start();
+	?>
+	
+	
+	<html>
+		<head>
+			<link rel="stylesheet" type="text/css" href="header.css">	
+		</head>
+	<body>
+		<?php 
+			include("header.php");
+		?>
+		
+		
+	</body>
+	</html>

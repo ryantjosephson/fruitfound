45a31fb9043f1b0f1fec75a2a2167c4688a83840
Ryan <ryantjosephson@gmail.com>
2020-03-27 13:53:47 -0600

    new

--- dao.php
+++ dao.php
@@ -1,4 +1,3 @@
-  
 <?php
 //require_once 'KLogger.php';
 
@@ -8,19 +7,19 @@ class Dao {
   private $dbname = 'heroku_a416b59d1326088';
   private $username = 'b6e186ca6a829b';
   private $password = '80469f78';
- // private $logger;
+  private $logger;
 
- // public function __construct() {
-  //  $this->logger = new KLogger("log.txt", KLogger::WARN);
- // }
+public function __construct() {
+    $this->logger = new KLogger("log.txt", KLogger::WARN);
+}
 
-  public function getConnection() {
-  //  try {
-       $connection = new PDO("mysql:host={$this->host};dbname={$this->dbname}", $this->username, $this->password);
-//    } catch (Exception $e) {
- //     $this->logger->LogError("Could not connect to the database: " . $e->getMessage());
- //     return null;
- //   }
+public function getConnection() {
+    try {
+       $connection = new PDO('mysql:host={$this->host};dbname={$this->dbname}', $this->username, $this->password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'');
+    } catch (Exception $e) {
+      $this->logger->LogError("Could not connect to the database: " . $e->getMessage());
+      return null;
+    }
     return $connection;
   }
   ?>

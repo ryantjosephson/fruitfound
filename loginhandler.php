dc7c3aeb393be01449d059c24af51a9ea1b5e75c
Ryan <ryantjosephson@gmail.com>
2020-03-27 13:28:34 -0600

    new

--- loginhandler.php
+++ loginhandler.php
@@ -1,21 +1,21 @@
 <?php
-  session_start();
+	session_start();
 	require_once 'dao.php';
 	
 	$dao = new Dao();
 	$connection = $dao->getConnection();
 	
-	$stmt = $connection->prepare("SELECT * FROM users WHERE UserName = ?");
-	$stmt->execute([$_POST['username']]);
+	$stmt = $connection->query("SELECT * FROM users WHERE UserName = '$_POST['username']'");
+//	$stmt->execute([$_POST['username']]);
 	$user = $stmt->fetch();
 
-if ($user && password_verify($_POST['password'], $user['password']))
-{
+if ($user && password_verify($_POST['password'], $user['password'])) {
     $_SESSION['auth'] = true;
     header("Location: https://fruitfound.herokuapp.com/index.php");
     exit;
 } else {
     $_SESSION['auth'] = false;
     $_SESSION['message'] = "Invalid username or password";
-    header("Location: https://fruitfound.herokuapp.com/login.php");}
+    header("Location: https://fruitfound.herokuapp.com/login.php");
+}
   ?>

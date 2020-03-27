eda85f7ee03e73c27788b4abb96f4c8cbe4c83db
Ryan <ryantjosephson@gmail.com>
2020-03-27 13:34:34 -0600

    new

--- dao.php
+++ dao.php
@@ -8,19 +8,19 @@ class Dao {
   private $dbname = 'heroku_a416b59d1326088';
   private $username = 'b6e186ca6a829b';
   private $password = '80469f78';
-  private $logger;
+ // private $logger;
 
  // public function __construct() {
   //  $this->logger = new KLogger("log.txt", KLogger::WARN);
  // }
 
   public function getConnection() {
-    try {
+  //  try {
        $connection = new PDO("mysql:host={$this->host};dbname={$this->dbname}", $this->username, $this->password);
-    } catch (Exception $e) {
-      $this->logger->LogError("Could not connect to the database: " . $e->getMessage());
-      return null;
-    }
+//    } catch (Exception $e) {
+ //     $this->logger->LogError("Could not connect to the database: " . $e->getMessage());
+ //     return null;
+ //   }
     return $connection;
   }
   ?>

32549a5b2e12d08f6688d0f893477ad87e5bb51e
Ryan <ryantjosephson@gmail.com>
2020-03-27 18:27:59 -0600

    new

--- signuphandler.php
+++ signuphandler.php
@@ -7,23 +7,19 @@
 	require_once ('dao.php');
 	$dao = new Dao();
 	$connection = $dao->getConnection();
-
-/* 	$stmt = $connection->prepare("INSERT INTO Users(FirstName,LastName, UserName, Password) 
-	VALUES (:firstname, :lastname, :username, :password)");
-	$stmt->bind_param(':firstname', $first);
-	$stmt->bind_param(':lastname', $last);
-	$stmt->bind_param(':username', $user);
-	$stmt->bind_param(':password', $pass);
 	
 	$first = filter_var($_POST['firstname']);
 	$last = filter_var($_POST['lastname']);
 	$user = filter_var($_POST['username']);
 	$pass = filter_var($_POST['password']);
-	$stmt->execute();
-	$useraccount = $connection->query("select * FROM users WHERE UserName = "$user"");
-	print_r($useraccount); */
 	
- if ($useraccount['username'] == $user ){
+	$stmt = $connection->prepare("INSERT INTO Users(FirstName,LastName, UserName, Password) VALUES (?,?,?,?)");
+	$stmt->execute([$first, $last, $user, $pass]);
+	
+	$useraccount = $connection->query("select * FROM users WHERE UserName = "$user"");
+	print_r($useraccount); 
+	
+ if ($useraccount['UserName'] == $user ){
     header("Location: https://fruitfound.herokuapp.com/login.php");
     exit;
 } else {

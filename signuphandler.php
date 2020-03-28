c5a291d2962988286b51f17ed45aead7e06fff8e
Ryan <ryantjosephson@gmail.com>
2020-03-27 20:54:29 -0600

    new

--- signuphandler.php
+++ signuphandler.php
@@ -18,7 +18,7 @@
 		
 		$stmt->bindValue(':username', $username);
 		$stmt->execute();
-		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+//		$row = $stmt->fetch(PDO::FETCH_ASSOC);
 	}
 		if($row['num']>0){
 			$_SESSION['message'] = "There is a duplicate Account";

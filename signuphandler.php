01b9432ec926a00e236a2bc3a77ba28639d89779
Ryan <ryantjosephson@gmail.com>
2020-03-27 18:35:10 -0600

    new

--- signuphandler.php
+++ signuphandler.php
@@ -13,11 +13,11 @@
 	$user = filter_var($_POST['username']);
 	$pass = filter_var($_POST['password']);
 	
-	$stmt = $connection->prepare("INSERT INTO users(FirstName, LastName, UserName, Password) VALUES (?,?,?,?)");
-	$stmt->execute([$first, $last, $user, $pass]);
+//	$stmt = $connection->prepare("INSERT INTO users(FirstName, LastName, UserName, Password) VALUES (?,?,?,?)");
+//	$stmt->execute([$first, $last, $user, $pass]);
 	
-	$useraccount = $connection->query("select * FROM users WHERE UserName = "$user"");
-	print_r($useraccount); 
+//	$useraccount = $connection->query("select * FROM users WHERE UserName = "$user"");
+//	print_r($useraccount); 
 	
  if ($useraccount['UserName'] == $user ){
     header("Location: https://fruitfound.herokuapp.com/login.php");

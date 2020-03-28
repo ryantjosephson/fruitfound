f7e7c20b5bc37dd58095127db689cdaf9d869327
Ryan <ryantjosephson@gmail.com>
2020-03-27 20:09:42 -0600

    new

--- signuphandler.php
+++ signuphandler.php
@@ -18,7 +18,7 @@
 		
 		$stmt->bindValue(':username', $username);
 		$stmt->execute();
-//		$row = $stmt->(fetchPDO::FETCH_ASSOC);
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
 	}
 		//if($row['num']>0){
 			$_SESSION['auth'] = false;

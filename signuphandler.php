4e3fded95958915959e460a3d3df4b5e84c0765a
Ryan <ryantjosephson@gmail.com>
2020-03-27 21:23:45 -0600

    new

--- signuphandler.php
+++ signuphandler.php
@@ -10,8 +10,11 @@
 	
 	if(isset($_POST['username'])){
 		
+	$first = $_POST['firstname']; //!empty($_POST['firstname']) ? trim($_POST['firstname'] : null;
+	$last = $_POST['lastname']; //!empty($_POST['lastname']) ? trim($_POST['lastname'] : null;	
 	$user = $_POST['username'];//!empty($_POST['username']) ? trim($_POST['username'] : null;
 	$pass = $_POST['password']; //!empty($_POST['password']) ? trim($_POST['password'] : null;
+
 		
 		$sql = "SELECT COUNT(UserName) AS num FROM users WHERE UserName = :username";
 		$stmt = $connection->prepare($sql);
@@ -26,30 +29,26 @@
 			exit;	
 		}
 		
+		$passwordhash = hash("sha256", "pass" . "fKd93Vmz!k*dAv5029Vkf9$3Aa")
 		
+		$sql = "INSERT INTO users (FirstName, LastName, UserName, Password) VALUES (:firstname, :lastname, :username, :password)";
+		$stmt = $connection->prepare($sql);
+		$stmt->bindValue(':firstname', $first);
+		$stmt->bindValue(':lastname', $last);
+		$stmt->bindValue(':username', $user);
+		$stmt->bindValue(':password', $passwordhash);
+		
+		$result = $stmt->execute();
+		
+		if($result){
+			$_Session['message'] = "Account Created, please sign in";
+			header("Location: https://fruitfound.herokuapp.com/index.php");
+			exit;
+		}else{
 			$_SESSION['message'] = "Account Creation Failed, please try again";
 			header("Location: https://fruitfound.herokuapp.com/index.php");
 			exit;
-	//	}
-	
-	
-	
-	
-/* 	$first = filter_var($_POST['firstname']);
-	$last = filter_var($_POST['lastname']);
-
-	
-	$stmt = $connection->prepare("INSERT INTO users(FirstName, LastName, UserName, Password) VALUES (?,?,?,?)");
-	$stmt->execute([$first, $last, $user, $pass])
-	
-	
-	
-	
-		header("Location: https://fruitfound.herokuapp.com/login.php");
-		exit;
-		}else{
-		
-	}	 */
+		}
 
   ?> 
   </pre>

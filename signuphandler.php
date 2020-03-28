a3f88748043c0922b53093b2f6c1cf670f1543f3
Ryan <ryantjosephson@gmail.com>
2020-03-27 19:02:31 -0600

    new

--- signuphandler.php
+++ signuphandler.php
@@ -6,27 +6,30 @@
 	session_start();
 	require_once ('dao.php');
 	$dao = new Dao();
-	$connection = $dao->getConnection();
 	
-	$first = filter_var($_POST['firstname']);
-	$last = filter_var($_POST['lastname']);
-	$user = filter_var($_POST['username']);
-	$pass = filter_var($_POST['password']);
+	try{
+		$connection = $dao->getConnection();
 	
-	$stmt = $connection->prepare("INSERT INTO users(FirstName, LastName, UserName, Password) VALUES (?,?,?,?)");
-	$stmt->execute([$first, $last, $user, $pass]);
+		$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$first = filter_var($_POST['firstname']);
+		$last = filter_var($_POST['lastname']);
+		$user = filter_var($_POST['username']);
+		$pass = filter_var($_POST['password']);
 	
-	$useraccount = $connection->query("select UserName FROM users WHERE UserName = "$user" LIMIT 1");
-	
- if ($useraccount['UserName'] = $user ){
-    header("Location: https://fruitfound.herokuapp.com/login.php");
-    exit;
-} else {
-    $_SESSION['auth'] = false;
-    $_SESSION['message'] = "Account Creation Failed, please try again";
-    header("Location: https://fruitfound.herokuapp.com/index.php");
-	exit;
-}
+		$stmt = $connection->prepare("INSERT INTO users(FirstName, LastName, UserName, Password) VALUES (?,?,?,?)");
+		if($stmt->execute([$first, $last, $user, $pass]);){
+		header("Location: https://fruitfound.herokuapp.com/login.php");
+		exit;
+		}else{
+			    $_SESSION['auth'] = false;
+		$_SESSION['message'] = "Account Creation Failed, please try again";
+		header("Location: https://fruitfound.herokuapp.com/index.php");
+		exit;
+		}
+	}	
+	catch(PDOException $e){
+		echo $e->getMessage);
+	}
   ?> 
   </pre>
   </body>

8419f574d4a7ce02778282b6f54954b5b6ca024d
Ryan <ryantjosephson@gmail.com>
2020-03-29 16:10:28 -0600

    new

--- editlocationhandler.php
+++ editlocationhandler.php
@@ -2,21 +2,32 @@
 	session_start();
 	require_once ('dao.php');
 	$dao = new Dao();
+	$connection = $dao->getConnection();
 	
 	if(isset($_POST['locationname'])){
-		$locationid = $_POST['locationid']; 
-		$location = $_POST['locationname']; 
-		$street = $_POST['street']; 
-		$city = $_POST['city'];
-		$state = $_POST['state']; 
-		$zip = $_POST['zip']; 
-		$phone = $_POST['phone'];
-		
-		$dao->$updateLocation($locationid, $location, $street, $city, $state, $zip, $phone);
-		//$dao->$updateLocation($_POST['locationid'],$_POST['locationname'],$_POST['street'],$_POST['city'],$_POST['state'],$_POST['zip'],$_POST['phone']);
-		$_SESSION['message'] = "Location has been updated.";
-		header("Location: https://fruitfound.herokuapp.com/account.php");
-		exit;
+	
+	$locationid = $_POST['locationid']; //!empty($_POST['firstname']) ? trim($_POST['firstname'] : null;
+	$location = $_POST['locationname']; //!empty($_POST['firstname']) ? trim($_POST['firstname'] : null;
+	$street = $_POST['street']; //!empty($_POST['lastname']) ? trim($_POST['lastname'] : null;	
+	$city = $_POST['city'];//!empty($_POST['username']) ? trim($_POST['username'] : null;
+	$state = $_POST['state']; //!empty($_POST['password']) ? trim($_POST['password'] : null;
+	$zip = $_POST['zip']; //!empty($_POST['password']) ? trim($_POST['password'] : null;
+	$phone = $_POST['phone']; //!empty($_POST['password']) ? trim($_POST['password'] : null;	
+	
+//	$connection->query("UPDATE  userlistings SET LocationName = $locationid, Street = $street, City = $city, State = $state, Zip = $zip, Phone = $phone WHERE LocationID = $locationid"); 
+	$sql = "UPDATE  userlistings SET LocationName = :location, Street = :street, City = :city, State = :state, Zip = :zip, Phone = :phone WHERE LocationID = :locationid"; 
+	$stmt = $connection->prepare($sql);
+	$stmt->bindParam(":location", $location);
+	$stmt->bindParam(":street", $street);
+	$stmt->bindParam(":city", $city);
+	$stmt->bindParam(":state", $state);
+	$stmt->bindParam(":zip", $zip);
+	$stmt->bindParam(":phone", $phone);
+	$stmt->bindParam(":locationid", $locationid);
+	$stmt->execute();
+	$_SESSION['message'] = "Location has been updated.";
+	header("Location: https://fruitfound.herokuapp.com/account.php");
+	exit;
 	}else {
 		$_SESSION['message'] = "Location has not been updated.";
 		header("Location: https://fruitfound.herokuapp.com/account.php");

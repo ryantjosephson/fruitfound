f233e08c99eb923cfcd803a52ff6d9ddbef6fd5b
Ryan <ryantjosephson@gmail.com>
2020-03-29 17:17:34 -0600

    new

--- header.php
+++ header.php
@@ -6,7 +6,16 @@
 		<h1 class="titletext">FruitFound!</h1>
 		
 	</div>
+	
+	<div class="header-middle">
+		<a class="search" href="login.php">Search for fruit on your area</a>
+	</div>
 	<div class="header-right">
+	<div>
 		<a class="loginpage" href="login.php">Login</a>
+	</div>
+	<div>
+		<a class="loginpage" href="logout.php">Logout</a>
+	</div>
 	</div>
 </header>

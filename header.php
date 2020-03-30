18ff5ae0c7f80a949baf2cdbc3ac336c3861013d
Ryan <ryantjosephson@gmail.com>
2020-03-29 18:17:34 -0600

    new

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 	<div class="header-right">
 	<div>
 		<?php
-		if($_SESSION['auth'] = false || !(isset($_SESSION['auth']))){
+		if($_SESSION['auth'] = false ){
 		echo "<a class='loginpage' href='login.php'>Login</a>";
 		}?>
 	</div>
--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
   session_destroy();
-  header("Location: https://fruitfound.herokuapp.com/login.php");
+  header("Location: https://fruitfound.herokuapp.com/index.php");
   exit;
 ?>

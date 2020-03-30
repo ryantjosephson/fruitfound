d6fa36ec54b24135f1fa9b203891498cbda74e73
Ryan Josephson <ryantjosephson>
2020-03-30 17:14:17 -0600

    new

--- addlocationhandler.php
+++ addlocationhandler.php
@@ -4,7 +4,7 @@
 	$dao = new Dao();
 	$connection = $dao->getConnection();
 	
-	if(isset($_POST['locationname'])&& isset($_POST['street']) && isset($_POST['city']) &&isset($_POST['state']) &&isset($_POST['zip']) &&isset($_POST['phone']) &&){
+	if(isset($_POST['locationname']) && isset($_POST['street']) && isset($_POST['city']) && isset($_POST['state']) && isset($_POST['zip']) && isset($_POST['phone'])){
 	
 	$location = htmlspecialchars($_POST['locationname']);
 	$street = htmlspecialchars($_POST['street']);

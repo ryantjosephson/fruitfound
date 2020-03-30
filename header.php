44cddc2b3d5a463892ee43e17d4e3e6de5c2c492
Ryan <ryantjosephson@gmail.com>
2020-03-29 18:21:23 -0600

    new

--- header.php
+++ header.php
@@ -14,12 +14,12 @@
 	<div class="header-right">
 	<div>
 		<?php
-		if($_SESSION['auth'] = false ){
+		if(!(isset($_SESSION['auth'])) || $_SESSION['auth'] = false ){
 		echo "<a class='loginpage' href='login.php'>Login</a>";
-		}?>
-	</div>
-	<div>
-		<a class="loginpage" href="logout.php">Logout</a>
+		}else{
+		echo "<a class='loginpage' href='logout.php'>Logout</a>";
+		}
+			?>
 	</div>
 	</div>
 </header>

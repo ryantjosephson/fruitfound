fe307c9352efa41968ce4f92196e91f091da9f31
Ryan <ryantjosephson@gmail.com>
2020-03-29 18:39:23 -0600

    new

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 		</div>
 		<div>
 				<?php
-		if (!isset($_SESSION['auth']) || !$_SESSION['auth']){
+		if (isset($_SESSION['auth']) || $_SESSION['auth']){
 		echo "<a class='loginpage' href='Account.php'>Manage Account</a>";
 		}
 			?>

a41404496fec5008c4a8d5a9cbba38ff2c2574f5
Ryan Josephson <ryantjosephson>
2020-03-30 14:59:44 -0600

    new

--- edit_location.php
+++ edit_location.php
@@ -31,8 +31,8 @@ session_start();
 	</div>
 	<div class="editformcontainer">
  		 <form method="POST" action="editlocationhandler.php">
-			<div class="newuserdiv"> <input type="hidden" id="locationid" name="locationid" value="<?php echo $_GET['id'];?>">
-			<div class="newuserdiv"> Location Name:</br> <input type="text" id="locationname" name="locationname" value="<?php echo $location['LocationName'];?>"> </div>
+			<div class="newuserdiv"> <input type="hidden" id="locationid" name="locationid" value="<?php echo "{$_GET['id']}";?>">
+			<div class="newuserdiv"> Location Name:</br> <input type="text" id="locationname" name="locationname" value="<?php echo "{$location['LocationName']}";?>"> </div>
 			<div class="newuserdiv"> Street:</br><input type="text" id="street" name="street" value="<?php echo "{$location['Street']}";?>"> </div>
 			<div class="newuserdiv"> City:</br> <input type="text" id="city" name="city" value="<?php echo "{$location['City']}";?>"> </div>
 			<div class="newuserdiv"> State:</br> <input type="text" id="state" name="state" value="<?php echo "{$location['State']}";?>"> </div>

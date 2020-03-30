748119b8a58260ee1b6e772269e1effa58e9b961
Ryan Josephson <ryantjosephson>
2020-03-30 17:31:59 -0600

    new

--- login.php
+++ login.php
@@ -22,14 +22,16 @@
 				unset($_SESSION['messsage']);
 			}
 		?>
+		<div class="editformcontainer">
 		<h2 id="title">Login</h2>
+		
 		<form method="POST" action="loginhandler.php">
 			
 			<div class="formdiv"> <Label for="username">Username: <input type="text" id="username" name="username" pattern="\[^A-Za-z0-9_@\.]|@{2,}|\.{5,}"> </div>
 			<div class="formdiv"> <Label for="password"> Password:&nbsp; <input type="password" id="password" name="password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"> </div>
 			<div> <input type="submit" value="Submit"></div>
 		</form>
-		
+		</div>
 		
 		
 	</body>
